995b62fbc2d6831ed943b42f6c234121539acf2c
arohao <119353048+arohao@users.noreply.github.com>
2025-06-03 22:46:20 -0400

    Add background color to button

--- vite-service-site/src/components/Nav.jsx
+++ vite-service-site/src/components/Nav.jsx
@@ -4,16 +4,18 @@ import logo from '../assets/logo.png';
 function Nav() {
   return (
     <nav>
-      <div className='logo-group'>
+      <a className='logo-group' href='../../index.html'>
         <img src={logo} className='logo' alt='logo' />
         <h3>Starlight Dental Clinic</h3>
-      </div>
+      </a>
       <div className='links-group'>
         <h5>Home</h5>
         <h5>Services</h5>
         <h5>About Us</h5>
         <h5>Contact</h5>
-        <h5>Book Appointment</h5>
+        <h5>
+          <button>Book Appointment</button>
+        </h5>
       </div>
     </nav>
   );
