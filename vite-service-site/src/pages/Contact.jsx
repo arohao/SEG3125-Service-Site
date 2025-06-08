a133ccb7e5cbcf24904662fe294b9a63e5ef8184
arohao <119353048+arohao@users.noreply.github.com>
2025-06-08 15:13:14 -0400

    Add responsive contact us page

--- vite-service-site/src/pages/Contact.jsx
+++ vite-service-site/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import {useNavigate} from 'react-router-dom';
 import '../styles/form.css';
 import '../styles/calendar.css';
+import '../styles/App.css';
 
 function Contact() {
   const navigate = useNavigate();
@@ -12,45 +13,50 @@ function Contact() {
   return (
     <>
       <div className='banner'></div>
-      <main className='row mt-5 gap-5'>
-        <div className='col-4'>
+      <main className='row mt-5 contact-section'>
+        <div className='col-12 col-md-6 mb-4'>
           <h1>Contact Us</h1>
-          <p style={{lineHeight: '1.8', fontSize: '13pt'}}> A beautiful smile starts with a healthy mouth. A great dental experience starts at Starlight Dental Clinic.     
-              Our dental team is dedicated to providing you with the exceptional service and quality dental care you deserve. Our patients benefit from our comprehensive dental services, modern technology, and appreciate our patient-centered approach. We are your partner in optimal oral health, and together, we’ll achieve your smile goals.
+          <p style={{ lineHeight: '1.8', fontSize: '13pt' }}>
+            A beautiful smile starts with a healthy mouth. A great dental experience starts at Starlight Dental Clinic.     
+            Our dental team is dedicated to providing you with the exceptional service and quality dental care you deserve. 
+            Our patients benefit from our comprehensive dental services, modern technology, and appreciate our patient-centered approach. 
+            We are your partner in optimal oral health, and together, we’ll achieve your smile goals.
           </p>
         </div>
-        <div className='col'>
+
+        <div className='col-12 col-md-6'>
           <form className='booking-form needs-validation' action="/booking-success" method="post" onSubmit={handleSubmit}>
-            <div>
+            <div className='mb-3'>
               <label htmlFor='name'>Full Name</label>
-              <input id='name' type='text' placeholder="Enter Your Full Name" className='form-control' required/>
+              <input id='name' type='text' placeholder="Enter Your Full Name" className='form-control' required />
             </div>
-            <div>
+            <div className='mb-3'>
               <label htmlFor='phone-number'>Phone Number</label>
-              <input id='phone-number' type='text' placeholder="Enter Your Phone Number" className='form-control' required/>
+              <input id='phone-number' type='text' placeholder="Enter Your Phone Number" className='form-control' required />
             </div>
-            <div>
+            <div className='mb-3'>
               <label htmlFor='email'>Email</label>
-              <input id='email' type='text' placeholder="Enter Your Email" className='form-control' required/>
+              <input id='email' type='text' placeholder="Enter Your Email" className='form-control' required />
             </div>
-            <div>
-              <label>Any Additional Information</label>
-              <textarea id='additional-info' className='form-control'/>
+            <div className='mb-3'>
+              <label htmlFor='additional-info'>Any Additional Information</label>
+              <textarea id='additional-info' className='form-control' />
             </div>
-            <div>
+            <div className='mb-3'>
               <label>Preferred Method of Contact</label>
-              <select id='contact-method' type='' placeholder="Choose" className='form-control' required defaultValue=''>
+              <select id='contact-method' className='form-control' required defaultValue=''>
                 <option disabled value=''>Choose</option>
-                <option value='initial_exam'>Email</option>
-                <option value='dental_cleaning'>Phone</option>
+                <option value='email'>Email</option>
+                <option value='phone'>Phone</option>
               </select>
             </div>
             <div>
-              <input type='submit' className='submit'/>
+              <input type='submit' className='submit btn btn-primary' />
             </div>
           </form>
         </div>
       </main>
+
     </>
   );
 }
